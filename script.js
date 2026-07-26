9eef98fce0cf501d8cbf84bfa79fb14554cdab58
XLBoss36 <ich.gaming123@web.de>
2026-07-26 19:29:38 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,31 +7,32 @@ const protectedContent = document.getElementById('protected-content');
 const errorMsg = document.getElementById('error-msg');
 
 async function hashString(text) {
-	const msgUint8 = new TextEncoder().encode(text);
-	
-	const hashBuffer = await crypto.subtle.digest('SHA-256', msgUint8);
-	const hashArray = Array.from(new Uint8Array(hashBuffer);
-	return hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+  const msgUint8 = new TextEncoder().encode(text);
+  
+  const hashBuffer = await crypto.subtle.digest('SHA-256', msgUint8);
+  
+  const hashArray = Array.from(new Uint8Array(hashBuffer));
+  return hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
 }
 
 
 submitBtn.addEventListener('click', async function() {
-	const eingabe = pinInput.value;
-	
-	if (eingabe.length !== 3) {
-		errorMsg.textContent = "please enter exactly 3 numbers!";
-		errorMsg.classList.remove('hidden');
-		return;
-	}
-	
-	const eingabeHash = await hashString(eingabe);
-	
-	if (eingabeHash === SECRET_HASH) {
-		loginScreen.classList.add('hidden');
-		protectedContent.classList.remove('hidden');
-	} else {
-		errorMsg.textContent = "wrong Code!";
-		errorMsg.classList.remove('hidden');
-		pinInput.value = '';
-	}
+  const eingabe = pinInput.value;
+
+  if (eingabe.length !== 3) {
+    errorMsg.textContent = "Bitte genau 3 Zahlen eingeben!";
+    errorMsg.classList.remove('hidden');
+    return;
+  }
+
+  const eingabeHash = await hashString(eingabe);
+
+  if (eingabeHash === SECRET_HASH) {
+    loginScreen.classList.add('hidden');
+    protectedContent.classList.remove('hidden');
+  } else {
+    errorMsg.textContent = "Falscher Code!";
+    errorMsg.classList.remove('hidden');
+    pinInput.value = '';
+  }
 });
